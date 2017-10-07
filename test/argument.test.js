f69baa2a80b52c284cd1e88a1f5923813ba5d1d7
othman853 <anuar.yasser@gmail.com>
2017-10-07 19:31:25 -0300

    WIP

--- test/argument.test.js
+++ test/argument.test.js
@@ -26,7 +26,7 @@ test('given builds an argument with a sentence P', t => {
   t.deepEqual(argument.P(1), 2)
 })
 
-test('given builds an argument with a sentence P(x) = 1 and a set S = {1, 2, 3}', t => {
+test('given builds an argument with a sentence P(x) = x + 1 and a set S = {1, 2, 3}', t => {
 
   const S = [1, 2, 3]
 
@@ -36,4 +36,28 @@ test('given builds an argument with a sentence P(x) = 1 and a set S = {1, 2, 3}'
 
   t.deepEqual(argument.set, [1, 2, 3])
   t.deepEqual(argument.P(1), 2)
+})
+
+test('given builds an argument with a sentence P(x) = x + 1, a sentence Q(x) = x + 2 and a set S = {1, 2, 3}', t => {
+
+  const S = [1, 2, 3]
+
+  const argument = given(
+    aSet(S),
+    aSentence('P', x => x + 1),
+    aSentence('Q', x => x + 2))
+
+  t.deepEqual(argument.set, [1, 2, 3])
+  t.deepEqual(argument.P(1), 2)
+  t.deepEqual(argument.Q(2), 4)
+})
+
+test('a sentece P(x) = x + 1 in S, executes correctly by using existsInGivenSet', t => {
+
+  const S = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  const argument = given(
+    aSet(S),
+    aSentence('P', existsInGivenSet(x => x * x - 2 - x)))
+
 })
